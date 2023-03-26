d0e9ad1a6667bc488120c4fd179c82b62637eb58
Deniss Dmitrijevs 221RDB322 <125139518+deniss-dmitrijevs@users.noreply.github.com>
2023-03-26 17:47:27 +0000

    fix #5

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def process_queries(queries):
             # else: # otherwise, just add it
             #     contacts.append(cur_query)
         elif cur_query.type == 'del':
-            contacts.del_obj(key = cur_query.number, value = cur_query.name)
+            contacts.del_obj(key = cur_query.number
             # for j in range(len(contacts)):
             #     if contacts[j].number == cur_query.number:
             #         contacts.pop(j)
